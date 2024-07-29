956fa4bef21256c9e6ebd86f61072b4ef93522bb
traxito <monteplay@gmail.com>
2024-07-29 15:01:54 +0200

    all TF files for Azure Monitor

---
+++ AzureMonitor/main.tf
@@ -0,0 +1,96 @@
+#Terraform code to create Storage account, analytics workspace and export logs to analytics
+
+resource azurerm_resource_group alias_rg {
+  name     = var.rg-name    
+  location = var.location   
+}
+
+resource "azurerm_storage_account" "alias_st" {
+  name                     = var.st-name    
+  resource_group_name      = var.rg-name 
+  location                 = var.location  
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+
+resource azurerm_log_analytics_workspace alias_log {
+  name                = var.log-name    
+  location            = var.location
+  resource_group_name = var.rg-name 
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+data "azurerm_subscription" "current" {
+}
+
+resource azurerm_monitor_diagnostic_setting alias_activity_logs {
+  name                       = var.diag-name
+  target_resource_id         = data.azurerm_subscription.current.id
+  storage_account_id         = azurerm_storage_account.alias_st.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.alias_log.id
+
+  enabled_log {
+    category = "Administrative"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "Security"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "ServiceHealth"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "Alert"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "Recommendation"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "Policy"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "Autoscale"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "ResourceHealth"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+}
+
---
+++ AzureMonitor/variables.tf
@@ -0,0 +1,24 @@
+variable "rg-name" {
+  type        = string
+  description = "Name of the Resource Group for terraform resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the Resource Group for terraform resources"
+}
+
+variable st-name {
+    type = string
+    description = "CAUTION only lower case!!"
+}
+
+variable log-name {
+    type = string
+    description = "Caution alphanumeric only! Name of the Analytics Workspace"
+}
+
+variable diag-name {
+    type = string
+    description = "Name of the monitor_diagnostic_setting"
+}
