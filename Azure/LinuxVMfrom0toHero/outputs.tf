e3eca2d92220fba0f37825c05ae7b4eaffc14d68
Alexandre Montesinos Cavallé <58102601+traxito@users.noreply.github.com>
2024-08-06 19:26:47 +0200

    Create outputs.tf

---
+++ Azure/LinuxVMfrom0toHero/outputs.tf
@@ -0,0 +1,21 @@
+information to display after resource creation
+
+output "rg-name" {
+  description = "outputs the rg name"
+  value       = azurerm_resource_group.rg-alias.name
+}
+
+output "st-name" {
+  description = "outputs the st name"
+  value       = azurerm_storage_account.st-alias.name
+}
+
+output "vm-name" {
+  description = "outputs the VM name"
+  value       = azurerm_linux_virtual_machine.TerraformLinuxVM.name
+}
+
+output "PIP" {
+  description = "outputs the PIP"
+  value       = azurerm_public_ip.PIP.ip_address
+}
